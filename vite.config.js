97e1cdb48fbc2619f6c77039c053c474ab869a5d
Arun Rao <yarun1995.ar@gmail.com>
2024-12-25 15:37:45 +0530

    Github Pages Setup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/loginAuthentication/",
   server: {
     proxy: {
-      "/jokes": "http://localhost:3000/"
+      "/jokes": "http://localhost:3000/",
     },
   },
   plugins: [react()],
-})
-
+});
