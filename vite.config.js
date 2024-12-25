991c2f48ecb86b1116a5aa713f8c54d71940cd3d
Arun Rao <yarun1995.ar@gmail.com>
2024-12-25 15:45:45 +0530

    Github Pages Setup Try 1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/loginAuthentication/",
-  server: {
-    proxy: {
-      "/jokes": "http://localhost:3000/",
-    },
-  },
   plugins: [react()],
 });
