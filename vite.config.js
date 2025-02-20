d94309c635cb56153d636ba066fa7c1a0fa7f2a5
Arun Rao <yarun1995.ar@gmail.com>
2025-02-21 03:03:14 +0530

    Deployement Issue Try Fixed 3

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure output directory is correct
+    emptyOutDir: true, // Clean old builds
+    target: "esnext", // Optimize for latest browsers
+  },
 });
